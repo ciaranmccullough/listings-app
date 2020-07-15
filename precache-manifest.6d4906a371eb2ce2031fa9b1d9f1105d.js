3cf512e3a77d7a898a264cd8508b24c38045b04f
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-07-15 17:13:53 +0100

    Updates

---
+++ precache-manifest.6d4906a371eb2ce2031fa9b1d9f1105d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0159e79a06e7e35c8047a6de90bd7d",
+    "url": "/listings-app/index.html"
+  },
+  {
+    "revision": "311b9b434dd786c8d821",
+    "url": "/listings-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "14a93510d04d72f91c7e",
+    "url": "/listings-app/static/js/2.a1708edf.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/listings-app/static/js/2.a1708edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311b9b434dd786c8d821",
+    "url": "/listings-app/static/js/main.fb4d9d6a.chunk.js"
+  },
+  {
+    "revision": "567ead7cd4662196936c",
+    "url": "/listings-app/static/js/runtime-main.af91c562.js"
+  }
+]);
